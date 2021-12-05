da24c936a2232987c9e536952fb67f21f977de25
Ashutosh Ranjan <ashutosh.svea@gmail.com>
2021-12-05 19:10:32 +0100

    Refactored

--- SecondHomework/Program.cs
+++ SecondHomework/Program.cs
@@ -5,6 +5,76 @@ namespace SecondHomework
 {
     class Program
     {
+        static void executeFirst()
+        {
+            int count;
+            //Console.WriteLine("Case 1");
+            Console.WriteLine("Hur många vi ar som ska gå på bio?");
+            if (!int.TryParse(Console.ReadLine(), out count))
+            {
+                Console.WriteLine("Invalid number of people entered. Try again.");
+            }
+            else
+            {
+                int totalCost = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"Enter the age of person {i + 1} - ");
+                    int age = getAge();
+                    int price = findTicketPriceBasedOnAge(age);
+                    totalCost += price;
+                }
+
+                Console.WriteLine($"Total Number of people: {count}");
+                Console.WriteLine($"Total Ticket price: {totalCost}");
+            }
+
+        }
+
+        static void executeSecond()
+        {
+            Console.WriteLine("Please enter some text");
+            string someText = Console.ReadLine();
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($" {i}. {someText}");
+                // add comma for all but last repetition. 
+                if (i != 10)
+                    Console.Write(",");
+                else
+                    Console.WriteLine("");
+            }
+        }
+        static void executeThird()
+        {
+            Console.WriteLine("Enter a sentence with atleast three words.");
+            string sentence = Console.ReadLine();
+            //check for nulls and empties..
+            if (sentence == null || sentence.Equals(String.Empty) || sentence.Trim().Equals(String.Empty))
+            {
+                Console.WriteLine("Invalid input.");
+                return;
+            }
+            List<string> words = new List<string>();
+
+            string[] wordArray = sentence.Split(' '); //split based on space
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+
+                string word = wordArray[i].Trim(); //remove leading and trailing whitespaces
+                if (!(word == null || word.Equals(String.Empty))) //skip multiple whitespace and empty strings as words. 
+                {
+                    words.Add(word); //add trimed and valid words in list
+                }
+
+            }
+            //Again check for atleast three words after trimming whitespaces.
+            if (words.Count < 3)
+                Console.WriteLine("Invalid sentence. Must contains atleast three words.");
+            else
+                Console.WriteLine($"Third word is {words[2]}");
+
+        }
         static int getAge()
         {
             int age;
@@ -49,7 +119,7 @@ namespace SecondHomework
         static void mainMenu()
         {
             Console.WriteLine("You have reached the main menu.");
-            Console.WriteLine("Enter choice number to continue...");
+            Console.WriteLine("Enter choice to continue...");
 
             do
             {
@@ -71,78 +141,21 @@ namespace SecondHomework
                             Environment.Exit(0);
                             break;
                         case 1:
-                            int count;
-                            //Console.WriteLine("Case 1");
-                            Console.WriteLine("Hur många vi ar som ska gå på bio?");
-                            if (!int.TryParse(Console.ReadLine(), out count))
-                            {
-                                Console.WriteLine("Invalid number of people entered. Try again.");
-                            }
-                            else
-                            {
-                                int totalCost = 0;
-                                for (int i = 0; i < count; i++)
-                                {
-                                    Console.WriteLine($"Enter the age of person {i + 1} - ");
-                                    int age = getAge();
-                                    int price = findTicketPriceBasedOnAge(age);
-                                    totalCost += price;
-                                }
-
-                                Console.WriteLine($"Total Number of people: {count}");
-                                Console.WriteLine($"Total Ticket price: {totalCost}");
-                            }
-
+                            executeFirst(); 
                             break;
-
                         case 2:
                             //Console.WriteLine("Case 2");
-                            Console.WriteLine("Please enter some text");
-                            string someText = Console.ReadLine();
-                            for (int i = 1; i <= 10; i++)
-                            {
-                                Console.Write($" {i}. {someText}");
-                                // add comma for all but last repetition. 
-                                if (i != 10)
-                                    Console.Write(",");
-                                else
-                                    Console.WriteLine("");
-                            }
+                            executeSecond();
                             break;
-
                         case 3:
-                            Console.WriteLine("Enter a sentence with atleast three words.");
-                            string sentence = Console.ReadLine();
-                            //check for nulls and empties..
-                            if (sentence == null || sentence.Equals(String.Empty) || sentence.Trim().Equals(String.Empty))
-                            {
-                                Console.WriteLine("Invalid input.");
-                                break;
-                            }
-                            List<string> words = new List<string>();
-
-                            string[] wordArray = sentence.Split(' '); //split based on space
-                            for (int i = 0; i < wordArray.Length; i++)
-                            {
-
-                                string word = wordArray[i].Trim(); //remove leading and trailing whitespaces
-                                if (!(word == null || word.Equals(String.Empty))) //skip multiple whitespace and empty strings as words. 
-                                {
-                                    words.Add(word); //add trimed and valid words in list
-                                }
-
-                            }
-                            //Again check for atleast three words after trimming whitespaces.
-                            if (words.Count < 3)
-                                Console.WriteLine("Invalid sentence. Must contains atleast three words.");
-                            else
-                                Console.WriteLine($"Third word is {words[2]}");
+                            executeThird();
                             break;
                         default:
                             Console.WriteLine("Please enter a valid choice");
                             break;
 
                     }
+
                 }
 
             } while (true);
